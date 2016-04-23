abe6d69eec8d29d7baf3f7a7d5914fcae57b9c5b
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2016-04-23 16:53:53 +0200

    user: Add profile section
    When the user clicks on its name, access a section where it can control its profile and make arrangements based on permissions.
    
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- index.php
+++ index.php
@@ -30,6 +30,9 @@ switch ($path) {
     case 'article':
         $controller = new App\Controllers\ArticleController($params);
         break;
+    case 'profile':
+        $controller = new App\Controllers\ErrorController(501);
+        break;
     case 'user':
         $controller = new App\Controllers\ErrorController(501);
         break;
